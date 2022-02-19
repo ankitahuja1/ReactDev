30f3971970c9e4277d2325f411a6f189444ade2e
ankitahuja1 <adxdxa@gmail.com>
2022-02-20 00:42:35 +0530

    Dynamo add

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './Header';
 import SimpleBottomNavigation from './SimBottomNavigation';
-import BlsCard from './BlsCard'
+import BlsCard from './BlsCard';
+import Dynamo from './Dynamic';
 ReactDOM.render(
-  <React.StrictMode>
-    <Header/>
-    <BlsCard />
-    <SimpleBottomNavigation/>
-  </React.StrictMode>,
+  <Dynamo/>,
   document.getElementById('root')
 );
